0f9d6231a0ae50f1ae9ff0231aad1161fbfeaf51
root <root@datalogger.home>
2023-08-10 13:15:59 -0300

    chore: fix akmain and mpumain loops.

--- akmain.py
+++ akmain.py
@@ -1,12 +1,8 @@
 from smbus import SMBus
 import ak8963 as ak
-from time import sleep, monotonic
 
-bus = SMBus(1)
-dev = ak.AK8963(bus, 0x0c)
+dev = ak.AK8963(SMBus(1), 0x0c)
 
 while True:
     print(dev.read_all())
-    sleep(0.02)
-print(end - start)
  
--- mpumain.py
+++ mpumain.py
@@ -1,20 +1,8 @@
 from smbus import SMBus
 import mpu9250 as mpu
-from time import sleep, monotonic
 
-bus = SMBus(1)
-imu = mpu.MPU9250(bus, 0x68)
+imu = mpu.MPU9250(SMBus(1), 0x68)
 
-print(f"Accel sens = {imu.afsv}")
-imu.afsv = mpu.AccelFSV4G()
-print(f"Accel sens = {imu.afsv}")
-
-print(f"Gyro sens = {imu.gfsv}")
-imu.gfsv = mpu.GyroFSV1000DPS()
-print(f"Gyro sens = {imu.gfsv}")
-
-for _ in range(100):
-#while True:
+while True:
     print(imu.read_all())
-    #sleep(0.02)
  
