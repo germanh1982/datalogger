5be0ce61e1a7f7eb5e9c481cfe8c37e643d72cd8
Germán Herrera <germanh1982@gmail.com>
2023-08-10 16:21:42 -0300

    chore: remove benchmarking stuff.

--- bmpmain.py
+++ bmpmain.py
@@ -1,13 +1,10 @@
 from bmp280 import BMP280
 from smbus import SMBus
-from time import sleep, monotonic
+from time import sleep
 
 dev = BMP280(SMBus(1), 0x76)
 
-start = monotonic()
-#while True:
-for _ in range(1000):
-    print(dev.read_all())
-    #sleep(0.05)
-end = monotonic()
-print(end - start)
+while True:
+    ts, temp, pres = dev.read_all()
+    print(pres)
+    sleep(0.05)
